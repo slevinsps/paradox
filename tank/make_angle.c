06c8e25a947917f1e546ba2f19653342af2dfb90
Cimpoes Konstantin <kostea221297@mail.ru>
2017-07-06 00:56:38 +0300

    refresh tank folder

--- tank/make_angle.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <math.h>
-
-float get_angle(int, int, int, int);
-
-float get_angle(int x1, int y1, int x2, int y2){
-    int angle = 360 / M_PI * (atan((y2 - y1) / (x2 - x1)));
-    return angle;
-}
